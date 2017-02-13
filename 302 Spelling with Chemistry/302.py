92c13f4bc784af43ff939aaf569d5e178b51191d
Teague Lander <tlander121@hotmail.com>
2017-02-12 23:07:10 -0800

    Basic python printing and base function

--- 302 Spelling with Chemistry/302.py
+++ 302 Spelling with Chemistry/302.py
@@ -1,3 +1,25 @@
 import sys;
+import csv;
 
-print("hello");
+with open('ptdata2.csv') as f:
+	reader = csv.reader(f)
+	element_list = list(reader)
+
+for row in element_list:
+	print row
+
+
+def elementify(i_string):
+
+	strlen = len(i_string)
+
+	for i in range(1, strlen):
+		part1 = i_string[0:i]
+		part2 = i_string[i:strlen]
+		
+		print part1
+		print part2
+		print " "
+		
+		
+elementify("hello")
