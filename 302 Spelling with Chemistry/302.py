059c6eaf7eb1ed03db9c9607724afec150178787
Teague Lander <tlander121@hotmail.com>
2017-02-13 00:45:14 -0800

    Completed challenge (without bonus)

--- 302 Spelling with Chemistry/302.py
+++ 302 Spelling with Chemistry/302.py
@@ -1,25 +1,56 @@
-import sys;
 import csv;
 
 with open('ptdata2.csv') as f:
 	reader = csv.reader(f)
 	element_list = list(reader)
-
-for row in element_list:
-	print row
-
+	del element_list[0]
 
 def elementify(i_string):
 
 	strlen = len(i_string)
+	elements = []
 
+	if strlen <= 2:
+		for element in element_list:
+			# print "Checking element ", element[1], " against ", i_string
+			if (i_string == element[1].lower().strip()):
+				# print "Got element ", element
+				return [element]
+		return None
+	
 	for i in range(1, strlen):
 		part1 = i_string[0:i]
 		part2 = i_string[i:strlen]
 		
-		print part1
-		print part2
-		print " "
+		result1 = elementify(part1)
+		result2 = elementify(part2)
 		
-		
-elementify("hello")
+		if result1 == None or result2 == None:
+			continue
+			
+		for element in result2:
+			result1.append(element)
+			
+		return result1
+
+def print_elementified(i_elements):
+	element_symbols = []
+	element_names = []
+	
+	if (type(i_elements) is list):
+		for element in i_elements:
+			element_symbols.append(element[1].strip())
+			element_names.append(element[2].strip())
+		print ''.join(element_symbols), " (", ', '.join(element_names), ")"
+	else:
+		print "Not able to build word!"
+	
+	
+while True:
+	string_input = raw_input("Type in a word:")
+	result = elementify(string_input)
+	print_elementified(result)
+
+
+
+
